17ff97eddb5fdac904418634b28078d4de029f93
simurai <simulus@gmail.com>
2012-02-06 02:56:54 +0100

    Changed it to dimming on hover

--- chrome/dribbble-dimmer.js
+++ chrome/dribbble-dimmer.js
@@ -1,25 +1,13 @@
 // add UI elements
 $('body').prepend('<div id="dd-mask"></div>');
 $('#single-img').append('<a href="#" id="dd-toggle"></a>');
-$('#dd-toggle').hide();
-
-// observe shot mouseovers, show toggle link
-$('#single-img').hover(
-  function() {
-    $('#dd-toggle').fadeIn('fast');  
-  },
-  function() {
-    $('#dd-toggle').fadeOut('fast');  
-  }   
-);
 
 // observe toggle mouseovers
-$('#dd-toggle').click(function(e) {
-  e.preventDefault();
-  if ($('#dd-mask').is(':visible')) {
-    $('#dd-mask').fadeOut('fast');  
+$('#dd-toggle').hover(
+  function(e) {
+    $('#dd-mask').fadeIn('fast');
+  },
+  function(e) {
+    $('#dd-mask').fadeOut('fast');
   }
-  else {
-    $('#dd-mask').fadeIn('fast');  
-  }
-});
+);
